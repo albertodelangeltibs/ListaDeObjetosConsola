6d8cbc92bd8cfc6ecd528946bbf06ab682c65980
Alberto <rockstar4400@gmail.com>
2019-04-01 17:06:46 -0600

    Lista Facturas 01

---
+++ XmlToObjectPrueba01/Factura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlToObjectPrueba01
+{
+    public class Factura
+    {
+        [XmlArray("PropiedadesFactura")]
+        public List<PropiedadesFactura> Facturas { get; set; }
+
+        //public PropiedadesFactura[] Factura { get; set; }
+    }
+}
---
+++ XmlToObjectPrueba01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace XmlToObjectPrueba01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Serializer ser = new Serializer();
+            string path = string.Empty;
+            string xmlInputData = string.Empty;
+            string xmlOutputData = string.Empty;
+
+            //string filepath = Directory.GetCurrentDirectory() + @"\Salida.xml";
+
+            // Convert xml to Object
+            path = Directory.GetCurrentDirectory() + @"\Salida.xml";
+            xmlInputData = File.ReadAllText(path);
+
+            Factura factura = ser.Deserialize<Factura>(xmlInputData);
+            
+            xmlOutputData = ser.Serialize<Factura>(factura);
+
+            //Console.WriteLine(filepath);
+            Console.ReadLine();
+        }
+    }
+}
